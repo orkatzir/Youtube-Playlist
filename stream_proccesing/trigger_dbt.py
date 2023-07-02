5bfc5a4760f05ba537e7bba8a0a986214b095313
unknown <orkatzir@gmail.com>
2023-07-02 21:46:44 +0300

    fixed bugs

--- stream_proccesing/trigger_dbt.py
+++ stream_proccesing/trigger_dbt.py
@@ -1,8 +1,9 @@
 from prefect import flow
-import os
+# import os
 
 @flow(name="Run DBT Model")
 def trigger_dbt_command():
     # os.system("cd C:\\Users\\Or\\Documents\\dbt\\dbt_p && conda activate dbt &&  dbt run")
     print("hiiiiiii")
-trigger_dbt_command()
+if __name__ == '__main__':    
+ trigger_dbt_command()
